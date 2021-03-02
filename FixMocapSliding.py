c78c38b534ce7d7d82d8f463e180be9b73f6e3de
Apixirli <10371857+Apixirli@users.noreply.github.com>
2021-03-02 22:57:53 +0800

    Get sliding bone by selecting
    Get sliding bone by selecting joint in viewport/outliner before running the script.
    Assign bone to a variable called selectedJoint
    Clear selection to be safe
    
    Replaced "Character1_Hips" with selectedJoint[0]

--- FixMocapSliding.py
+++ FixMocapSliding.py
@@ -5,21 +5,23 @@
 
 import maya.cmds
 
+#get selected sliding bone
+selectedJoint = cmds.ls(sl=True)
+cmds.select(clear=True)
+
 #create fix locator
 cmds.spaceLocator(name="fixLoc")
 cmds.scale (10,10,10, "fixLoc")
 
-#FIND AND REPLACE "Character1_Hips" with Sliding Bone Name#
-
 #parent constraint fix locator to sliding bone, unparent to get bone location before sliding
-cmds.parentConstraint ("Character1_Hips","fixLoc")
+cmds.parentConstraint (selectedJoint[0],"fixLoc")
 cmds.parent ("fixLoc_parentConstraint1", removeObject= True)
 
 #constraint sliding bone to locator to fix sliding
-cmds.parentConstraint ("fixLoc","Character1_Hips", sr=["x","y","z"])
+cmds.parentConstraint ("fixLoc",selectedJoint[0], sr=["x","y","z"])
 
 #set weight from current frame to "1", weight from previous frame to "0"
-cmds.disconnectAttr ("Character1_Hips.blendParent1","pairBlend1.weight") 
+cmds.disconnectAttr ("%s.blendParent1"%selectedJoint[0],"pairBlend1.weight") 
 cmds.select ("pairBlend1")
 cmds.setKeyframe (v=1, at="weight")
 currTime = cmds.currentTime(q=1)
